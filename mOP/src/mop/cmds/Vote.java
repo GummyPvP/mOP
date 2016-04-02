2da4d51320c18318d697605a4fc2581964d00b8b
xXkguyXx <jkacuties@gmail.com>
2016-04-02 13:05:48 -0400

    Updated vote links

--- mOP/src/mop/cmds/Vote.java
+++ mOP/src/mop/cmds/Vote.java
@@ -13,11 +13,7 @@ public class Vote implements CommandExecutor {
 		if (cmd.getName().equalsIgnoreCase("Vote")) {
 			sender.sendMessage(" ");
 			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l» &aVote at these fine websites to get rewards!"));
-			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l» &ahttp://minecraft-server-list.com/server/344316/vote/"));
-			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l» &ahttp://www.planetminecraft.com/server/gummymc/vote/"));
-			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l» &ahttps://www.minestatus.net/144708-gummymc"));
-			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l» &ahttp://minecraftservers.org/server/340826"));
-			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l» &ahttp://topg.org/Minecraft/in-429803"));
+			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l» &ahttp://gummypvp.com/vote"));
 			
 			sender.sendMessage(" ");
 		}
